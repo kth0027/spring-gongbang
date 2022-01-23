62e9d0f7664018a73eaab0419a84428438d61340
valorjj <valorjj@gmail.com>
2022-01-23 13:49:44 +0900

    init by jj

--- src/main/java/com/ezen/controller/MemberController.java
+++ src/main/java/com/ezen/controller/MemberController.java
@@ -1,15 +1,18 @@
 package com.ezen.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/member")
 @Controller
+@RequestMapping(value = "/member")
 public class MemberController {
 
     @GetMapping("/signup")
-    public String login() {
+    public String signup() {
         return "member/signup";
     }
+
+
 }

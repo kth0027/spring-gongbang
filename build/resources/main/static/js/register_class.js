55a742c366e013b33bbe2de25a3a7491cf20cd84
505 <wlwlgns92@gmail.com>
2022-01-25 18:22:24 +0900

    jh

---
+++ build/resources/main/static/js/register_class.js
@@ -0,0 +1,119 @@
+$(function(){
+
+    $("ul.tabs li").click(function(){
+        var tabId = $(this).attr("data-tab");
+
+        $("ul.tabs li").removeClass("current");
+        $(".tab-content").removeClass("current");
+
+        $(this).addClass("current");
+        $("#"+ tabId).addClass("current");
+    })
+
+});
+
+var input = document.getElementById("pinput");
+var initLabel = document.getElementById("plabel");
+
+input.addEventListener("change", (event) => {
+  const files = changeEvent(event);
+  handleUpdate(files);
+});
+
+initLabel.addEventListener("mouseover", (event) => {
+  event.preventDefault();
+  const label = document.getElementById("plabel");
+  label?.classList.add("plabel--hover");
+});
+
+initLabel.addEventListener("mouseout", (event) => {
+  event.preventDefault();
+  const label = document.getElementById("plabel");
+  label?.classList.remove("plabel--hover");
+});
+
+document.addEventListener("dragenter", (event) => {
+  event.preventDefault();
+  console.log("dragenter");
+  if (event.target.className === "inner") {
+    event.target.style.background = "#616161";
+  }
+});
+
+document.addEventListener("dragover", (event) => {
+  console.log("dragover");
+  event.preventDefault();
+});
+
+document.addEventListener("dragleave", (event) => {
+  event.preventDefault();
+  console.log("dragleave");
+  if (event.target.className === "pinner") {
+    event.target.style.background = "#3a3a3a";
+  }
+});
+
+document.addEventListener("drop", (event) => {
+  event.preventDefault();
+  console.log("drop");
+  if (event.target.className === "pinner") {
+    const files = event.dataTransfer?.files;
+    event.target.style.background = "#3a3a3a";
+    handleUpdate([...files]);
+  }
+});
+
+function changeEvent(event){
+  const { target } = event;
+  return [...target.files];
+};
+
+function handleUpdate(fileList){
+  const preview = document.getElementById("preview");
+
+  fileList.forEach((file) => {
+    const reader = new FileReader();
+    reader.addEventListener("load", (event) => {
+      const img = el("img", {
+        className: "embed-img",
+        src: event.target?.result,
+      });
+      const imgContainer = el("div", { className: "container-img" }, img);
+      preview.append(imgContainer);
+    });
+    reader.readAsDataURL(file);
+  });
+};
+
+function el(nodeName, attributes, ...children) {
+  const node =
+    nodeName === "fragment"
+      ? document.createDocumentFragment()
+      : document.createElement(nodeName);
+
+  Object.entries(attributes).forEach(([key, value]) => {
+    if (key === "events") {
+      Object.entries(value).forEach(([type, listener]) => {
+        node.addEventListener(type, listener);
+      });
+    } else if (key in node) {
+      try {
+        node[key] = value;
+      } catch (err) {
+        node.setAttribute(key, value);
+      }
+    } else {
+      node.setAttribute(key, value);
+    }
+  });
+
+  children.forEach((childNode) => {
+    if (typeof childNode === "string") {
+      node.appendChild(document.createTextNode(childNode));
+    } else {
+      node.appendChild(childNode);
+    }
+  });
+
+  return node;
+}

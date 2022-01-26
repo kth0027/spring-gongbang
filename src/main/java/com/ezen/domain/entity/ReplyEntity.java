81b11fe0d4f7cb1a58b09d6fd65d9e898eadb0cf
505 <wlwlgns92@gmail.com>
2022-01-26 17:37:53 +0900

    조지훈

--- src/main/java/com/ezen/domain/entity/ReplyEntity.java
+++ src/main/java/com/ezen/domain/entity/ReplyEntity.java
@@ -13,27 +13,27 @@ import javax.persistence.*;
 @Builder
 public class ReplyEntity extends BaseTimeEntity{
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name="replyNo")
-//    private int replyNo;
-//
-//    @Column(name="replyContent")
-//    private String replyContent;
-//
-    // 후기 / 문의 로 구분지어 하나로 쓸까 생각중
-//    @Column(name="replyCategory")
-//    private int replyCategory;
-//
-//
-//    @ManyToOne
-//    @JoinColumn(name="roomNo") // 해당 필드의 이름[컬럼 = 열 = 필드]
-//    private RoomEntity roomEntity;
-//
-//    @ManyToOne
-//    @JoinColumn(name="memberNo") // 해당 필드의 이름[컬럼 = 열 = 필드]
-//    private MemberEntity memberEntity;
-//
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="replyNo")
+    private int replyNo;
+
+    @Column(name="replyContent")
+    private String replyContent;
+
+     // 후기 / 문의 로 나누어 쓸까 생각
+    @Column(name="replyCategory")
+    private int replyCategory;
+
+
+    @ManyToOne
+    @JoinColumn(name="roomNo") // 해당 필드의 이름[컬럼 = 열 = 필드]
+    private RoomEntity roomEntity;
+
+    @ManyToOne
+    @JoinColumn(name="memberNo") // 해당 필드의 이름[컬럼 = 열 = 필드]
+    private MemberEntity memberEntity;
+
 
 
 

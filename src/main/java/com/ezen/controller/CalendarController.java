d455495ed212ca931e45cc6c1701ae552f826d69
valorjj <valorjj@gmail.com>
2022-02-04 00:51:03 +0900

    jj

--- src/main/java/com/ezen/controller/CalendarController.java
+++ src/main/java/com/ezen/controller/CalendarController.java
@@ -1,9 +1,16 @@
 package com.ezen.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Calendar;
 
 @Controller
+@RequestMapping(value = "calendar.do", method = RequestMethod.GET)
 public class CalendarController {
 
+    Calendar cal = Calendar.getInstance();
+    DateData calednarData;
 
 }

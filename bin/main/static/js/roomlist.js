284bd0843ad0a1e725f9fc1c135e6d90f1f8478f
kth0027 <so_dream@naver.com>
2022-02-11 17:49:39 +0900

    kth0211 - 머지전 브랜치

---
+++ bin/main/static/js/roomlist.js
@@ -0,0 +1,40 @@
+// 클래스 지우기
+function rdelete(roomNo){
+alert(roomNo);
+    $.ajax({
+        url:"/admin/delete",
+        data:{"roomNo":roomNo},
+        success:function(data){
+            if(data==1){
+                location.href ="/admin/adminlist";
+            }
+
+        }
+    });
+
+}
+
+
+
+
+// 클래스 상태변경
+function activeupdate(roomNo, active) {
+alert(roomNo);
+alert(active);
+    $.ajax({
+            url:"/admin/activeupdate",
+            data:{"roomNo":roomNo, "active":active},
+            success:function(data){
+                if(data==1){
+                    location.href ="/admin/adminlist";
+                } else {
+                $("#activemsg").html("현재 동일한 상태입니다.");
+                }
+
+            }
+
+
+        });
+
+
+}

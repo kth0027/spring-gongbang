4d7a510b4998ade2889b1bb2ddb3e050db915e5e
kth0027 <so_dream@naver.com>
2022-02-16 09:54:40 +0900

    20220216 - 작업중 (회원 수정 오류 필요)

--- src/main/java/com/ezen/domain/entity/repository/MemberRepository.java
+++ src/main/java/com/ezen/domain/entity/repository/MemberRepository.java
@@ -9,6 +9,8 @@ public interface MemberRepository extends JpaRepository <MemberEntity, Integer> 
 
     // 엔티티 검색 findby필드명
     Optional<MemberEntity> findBymemberId(String memberId);
+    // 멤버 No 찾기
+    Optional<MemberEntity> findBymemberNo(String memberNo);
     //이메일 검사
     Optional<MemberEntity> findBymemberEmail(String memberEmail);
 }
